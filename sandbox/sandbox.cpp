ecb3dee8040bf481764faab85cc15d6b93dd5e73
callumpoole <14567682+callumpoole@users.noreply.github.com>
2023-07-08 22:10:20 +0100

    Playing with collision, box to box working and circle to circle too.

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -16,16 +16,59 @@
 #include "renderer_testing.h"
 #include "font_testing.h"
 
+#include "poole/physics/collision_2D/box_collider_2D.h"
+#include "poole/physics/collision_2D/circle_collider_2D.h"
+#include "poole/physics/collision_2D/collision_manager_2D.h"
+
+std::shared_ptr<Poole::BoxCollider2D> box1;
+std::shared_ptr<Poole::BoxCollider2D> box2;
+//std::shared_ptr<Poole::BoxCollider2D> box3;
+std::shared_ptr<Poole::CircleCollider2D> circle1;
+std::shared_ptr<Poole::CircleCollider2D> circle2;
+
 void Sandbox::BeginApp()
 {
     //RendererTesting::Init();
     //FontTesting::Init();
+
+    box1 = Poole::ColliderManager2D::MakeCollider<Poole::BoxCollider2D>(fvec2{  0.2, 0.5 }, fvec2{ 0.25, 0.1 }, PI / 3);
+    box2 = Poole::ColliderManager2D::MakeCollider<Poole::BoxCollider2D>(fvec2{ -0.4, 0.8 }, fvec2{ 0.1, 0.35 }, PI / 8);
+//    box3 = Poole::ColliderManager2D::MakeCollider<Poole::BoxCollider2D>(fvec2{ 0.4, -0.8 }, fvec2{ 0.15, 0.3 }, -PI / 8);
+
+    circle1 = Poole::ColliderManager2D::MakeCollider<Poole::CircleCollider2D>(fvec2{ -0.2, -0.5 }, 0.1f);
+    circle2 = Poole::ColliderManager2D::MakeCollider<Poole::CircleCollider2D>(fvec2{ -0.9, 0.8 }, 0.2f);
 }
+
+float aa = 0.0f;
+float bb = 1.2f;
 
 void Sandbox::UpdateApp(float deltaTime)
 {
     //RendererTesting::Tick(deltaTime);
     //FontTesting::Tick(deltaTime);
+
+    using namespace Poole;
+
+    const fvec2 mouseToScene = Input::GetMousePositionToScene();
+
+    box1->DebugDraw();
+    box2->DebugDraw();
+    //box3->DebugDraw();
+
+
+    static_cast<BoxCollider2D*>(box2.get())->SetPosition(mouseToScene);
+//    static_cast<BoxCollider2D*>(box3.get())->SetPosition(mouseToScene + fvec2{-0.8f, 0.8f});
+
+    aa += deltaTime / 10.f;
+    bb += deltaTime / 20.f;
+
+    static_cast<BoxCollider2D*>(box1.get())->SetRadians(aa);
+    static_cast<BoxCollider2D*>(box2.get())->SetRadians(bb);
+
+
+    circle1->DebugDraw();
+    circle2->DebugDraw();
+    static_cast<CircleCollider2D*>(circle2.get())->SetPosition(mouseToScene + fvec2{0.5,0.5});
 }
 
 void Sandbox::EndApp()
